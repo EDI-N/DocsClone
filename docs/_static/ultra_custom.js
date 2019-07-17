a4a965923fc751443fff34bd3ead7c77ed4ca5e3
Kolya Bezgin <bezginn@edi-n.com>
2019-07-17 14:11:19 +0300

    to tops

--- docs/_static/ultra_custom.js
+++ docs/_static/ultra_custom.js
@@ -8,17 +8,3 @@
     d.getElementsByTagName("head")[0].appendChild(s);
 })
 ();
-
-// ===== Scroll to Top ==== 
-$(window).scroll(function() {
-    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-        $('#topchik').fadeIn(200);    // Fade in the arrow
-    } else {
-        $('#topchik').fadeOut(200);   // Else fade out the arrow
-    }
-});
-$('#topchik').click(function() {      // When arrow is clicked
-    $('body,html').animate({
-        scrollTop : 0                       // Scroll to top of body
-    }, 500);
-});
