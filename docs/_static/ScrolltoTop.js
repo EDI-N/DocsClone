e0fcc0cdb716ca28e2243a8e5dd8a8a240027e82
Kolya Bezgin <bezginn@edi-n.com>
2019-07-17 18:25:51 +0300

    dwefef

--- docs/_static/ScrolltoTop.js
+++ docs/_static/ScrolltoTop.js
@@ -1,15 +1,15 @@
 // ===== Scroll to Top ==== 
-(function () {
-    $(window).scroll(function() {
-        if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-            $('#topchik').fadeIn(200);    // Fade in the arrow
-        } else {
-            $('#topchik').fadeOut(200);   // Else fade out the arrow
-        }
-    });
-    $('#topchik').click(function() {      // When arrow is clicked
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) { // If page is scrolled more than 50px
+        $('#topchik').fadeIn(200); // Fade in the arrow
+    } else {
+        $('#topchik').fadeOut(200); // Else fade out the arrow
+    }
+});
+(function() {
+    $('#topchik').click(function() { // When arrow is clicked
         $('body,html').animate({
-            scrollTop : 0                       // Scroll to top of body
+            scrollTop: 0 // Scroll to top of body
         }, 500);
     });
 })
