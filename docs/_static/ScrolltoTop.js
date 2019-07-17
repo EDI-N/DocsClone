c39e9906a1935ba570bbbf3bd2b7bd5d858adee8
Kolya Bezgin <bezginn@edi-n.com>
2019-07-17 17:57:10 +0300

    topaem

--- docs/_static/ScrolltoTop.js
+++ docs/_static/ScrolltoTop.js
@@ -1,13 +1,16 @@
 // ===== Scroll to Top ==== 
-$(window).scroll(function() {
-    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-        $('#topchik').fadeIn(200);    // Fade in the arrow
-    } else {
-        $('#topchik').fadeOut(200);   // Else fade out the arrow
-    }
-});
-$('#topchik').click(function() {      // When arrow is clicked
-    $('body,html').animate({
-        scrollTop : 0                       // Scroll to top of body
-    }, 500);
-});
+(function () {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+            $('#topchik').fadeIn(200);    // Fade in the arrow
+        } else {
+            $('#topchik').fadeOut(200);   // Else fade out the arrow
+        }
+    });
+    $('#topchik').click(function() {      // When arrow is clicked
+        $('body,html').animate({
+            scrollTop : 0                       // Scroll to top of body
+        }, 500);
+    });
+})
+();
