21e15130d2cebfe16ea940d551c2fdb1ff9ec9dd
Kolya Bezgin <bezginn@edi-n.com>
2019-07-18 11:12:15 +0300

    wrong js

--- docs/_static/ScrolltoTop.js
+++ docs/_static/ScrolltoTop.js
@@ -13,19 +13,23 @@
 //});
 
 
-window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("topchik").style.display = "block";
-  } else {
-    document.getElementById("topchik").style.display = "none";
-  }
-}
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-} 
+(function () {
+    window.onscroll = function() {scrollFunction()};
 
+    function scrollFunction() {
+      if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("topchik").style.display = "block";
+      } else {
+        document.getElementById("topchik").style.display = "none";
+      }
+    }
+    
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+      document.body.scrollTop = 0; // For Safari
+      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    } 
+})
+();
