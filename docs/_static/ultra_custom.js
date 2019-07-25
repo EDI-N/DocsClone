005e032b9088f7c437eae1ebbb06228c79bcadd6
Kolya Bezgin <bezginn@edi-n.com>
2019-07-25 13:34:39 +0300

    notif

--- docs/_static/ultra_custom.js
+++ docs/_static/ultra_custom.js
@@ -7,4 +7,15 @@
     s.setAttribute("jv-id", "iEUmonxahY");
     d.getElementsByTagName("head")[0].appendChild(s);
 })
+
+// onesignal plugin
+
+(function () {
+    d = document;
+    s = d.createElement("script");
+    s.src = "//cdn.onesignal.com/sdks/OneSignalSDK.js";
+    s.async = 1;
+    s.setAttribute("appId", "7b5d5a38-eca8-4bfd-a89b-623c20fbd404");
+    d.getElementsByTagName("head")[0].appendChild(s);
+})
 ();
