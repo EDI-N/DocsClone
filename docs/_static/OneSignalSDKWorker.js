c933f5036929c772ffb39b8a329c6a0d70c90aa7
Kolya Bezgin <bezginn@edi-n.com>
2019-07-26 15:22:14 +0300

    sdfsdfsdf

--- docs/_static/OneSignalSDKWorker.js
+++ docs/_static/OneSignalSDKWorker.js
@@ -1,10 +1,11 @@
 // onesignal
-(function () {
-    d = document;
-    s = d.createElement("script");
-    s.src = "//cdn.onesignal.com/sdks/OneSignalSDK.js";
-    s.async = 1;
-    s.setAttribute("appId", "7b5d5a38-eca8-4bfd-a89b-623c20fbd404");
-    d.getElementsByTagName("head")[0].appendChild(s);
-})
-();
+    <link rel="manifest" href="/manifest.json" />
+    <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
+    <script>
+    var OneSignal = window.OneSignal || [];
+    OneSignal.push(function() {
+        OneSignal.init({
+        appId: "7b5d5a38-eca8-4bfd-a89b-623c20fbd404",
+        });
+    });
+    </script>
